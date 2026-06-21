44bda30bb9a7c4bd7066a07f778e7632f96c2105
Daniel <daniel.ivanfi@playngo.com>
2026-06-21 18:58:26 +0200

    added preloader

--- app/app.vue
+++ app/app.vue
@@ -14,10 +14,34 @@ useHead({
 
 // PhotoSwipe CSS
 import 'photoswipe/style.css'
+
+// LOADER LOGIC
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const loading = ref(false)
+const router = useRouter()
+
+router.beforeEach(() => {
+  loading.value = true
+})
+
+router.afterEach(() => {
+  setTimeout(() => {
+    loading.value = false
+  }, 200) // kis delay, hogy ne villogjon
+})
 </script>
 
-
 <template>
+  <!-- GLOBAL LOADER -->
+  <div
+      v-if="loading"
+      class="fixed inset-0 flex items-center justify-center bg-black/40 backdrop-blur-sm z-[9999]"
+  >
+    <div class="loader"></div>
+  </div>
+
   <div class="font-oswald">
     <Header />
 
@@ -28,3 +52,18 @@ import 'photoswipe/style.css'
     <Footer />
   </div>
 </template>
+
+<style>
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 4px solid white;
+  border-top-color: transparent;
+  border-radius: 50%;
+  animation: spin 0.7s linear infinite;
+}
+
+@keyframes spin {
+  to { transform: rotate(360deg); }
+}
+</style>
