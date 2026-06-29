ec573d5d7a747a177e863ae9d1420891641a54b0
Daniel <daniel.ivanfi@playngo.com>
2026-06-29 16:15:52 +0200

    Removed loader

--- app/app.vue
+++ app/app.vue
@@ -10,34 +10,9 @@ useHead({
 
 // PhotoSwipe CSS
 import 'photoswipe/style.css'
-
-// LOADER LOGIC
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-
-const loading = ref(false)
-const router = useRouter()
-
-router.beforeEach(() => {
-  loading.value = true
-})
-
-router.afterEach(() => {
-  setTimeout(() => {
-    loading.value = false
-  }, 200)
-})
 </script>
 
 <template>
-  <!-- GLOBAL LOADER -->
-  <div
-      v-if="loading"
-      class="fixed inset-0 flex items-center justify-center bg-black/40 backdrop-blur-sm z-[9999]"
-  >
-    <div class="loader"></div>
-  </div>
-
   <!-- GLOBAL FONT SYSTEM: Inter Medium -->
   <div class="font-body font-medium">
     <Header />
@@ -51,20 +26,6 @@ router.afterEach(() => {
 </template>
 
 <style>
-/* LOADER */
-.loader {
-  width: 48px;
-  height: 48px;
-  border: 4px solid white;
-  border-top-color: transparent;
-  border-radius: 50%;
-  animation: spin 0.7s linear infinite;
-}
-
-@keyframes spin {
-  to { transform: rotate(360deg); }
-}
-
 /* GLOBAL HEADINGS  */
 h1 {
   @apply font-lato font-black uppercase text-[48px] leading-[52px] tracking-[0.2px] text-[#404040] mb-10;
@@ -78,7 +39,6 @@ h3 {
 p {
   @apply text-lg leading-relaxed text-[#404040] mb-6 text-left;
 }
-
 
 /* Two-line H2 structure */
 h2 span:first-child {
